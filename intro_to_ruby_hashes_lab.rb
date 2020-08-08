389670db62f1998ee7881446816273ff5ef6fac1
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-08 02:36:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,14 +24,16 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-   {:railroads => {
+   {
+     :railroads => {
     :pieces => 4,
     :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50
     },
     :names => {},
-  }}
+  }
+}
 end
 
 def monopoly_with_fourth_tier
